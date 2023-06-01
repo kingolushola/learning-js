9c994961f496d23ad0a6674ff01fdf08c50a3ad8
kingolushola <olusholaabiodun01@gmail.com>
2023-06-01 14:03:25 +0100

    update on js

--- app.js
+++ app.js
@@ -1,175 +1,303 @@
-// console.log('hello world');
-// console.log('welcome to class');
-// console.log('first class');
+// // console.log('hello world');
+// // console.log('welcome to class');
+// // console.log('first class');
 
 
-// // JAVASCRIPT VARAIABLES AND VALUES
-// let x = 2;
-// const y = 4;
-// console.log(x);
-// console.log(y);
+// // // JAVASCRIPT VARAIABLES AND VALUES
+// // let x = 2;
+// // const y = 4;
+// // console.log(x);
+// // console.log(y);
 
-// // // Convention in naming variables(var)
-// // // have a logical var name
-// // const b = 'Mubarak'
-// // const Firstname = 'Mubarak'
-// // // compound names - camelcasing
-// // const lastName = 'Funsho'
-// // // // illegal ( you can not start with number but you can start with dolars($)
-// // // const 1year = 2003(wrong)
-// // // const $year = 2003(right)
+// // // // Convention in naming variables(var)
+// // // // have a logical var name
+// // // const b = 'Mubarak'
+// // // const Firstname = 'Mubarak'
+// // // // compound names - camelcasing
+// // // const lastName = 'Funsho'
+// // // // // illegal ( you can not start with number but you can start with dolars($)
+// // // // const 1year = 2003(wrong)
+// // // // const $year = 2003(right)
 
-// // // let country = 'nigeria'
-// // // const state = 'lagos'
-// // // console.log(country);
-// // // console.log(state);
+// // // // let country = 'nigeria'
+// // // // const state = 'lagos'
+// // // // console.log(country);
+// // // // console.log(state);
 
-// const country = 'Nigeria'
-// let state = 'lagos'
-// console.log(country);
-// console.log(state);
+// // const country = 'Nigeria'
+// // let state = 'lagos'
+// // console.log(country);
+// // console.log(state);
 
-// // javascript data types-( primitive data type amd complex datatype)
-// // strings- text-quotes-strings
-// const firstName = "john";
-// const lastName = 'Doe';
-// const middleName = "owen";
-// const currentJob = "product Manager";
-// const email = "olusholaabiodun01@gmail.com";
-// //  string concatenation - joining of string
-// const fullname = firstName + " " + middleName + " " +  lastName;
-// console.log(fullname);
-// // you can message me on this email,olusholaabiodun01@gmail.com
-// const Email = " you can message me on this email " + email
-// console.log(Email);
-// // Getting Characters -
-// console.log(middleName[1]);
-// //  string lenght
-// console.log(firstName.length);
-// // String Method
-// // transform
-// console.log(fullname.toUpperCase());
-// console.log(fullname.toLowerCase());
+// // // javascript data types-( primitive data type amd complex datatype)
+// // // strings- text-quotes-strings
+// // const firstName = "john";
+// // const lastName = 'Doe';
+// // const middleName = "owen";
+// // const currentJob = "product Manager";
+// // const email = "olusholaabiodun01@gmail.com";
+// // //  string concatenation - joining of string
+// // const fullname = firstName + " " + middleName + " " +  lastName;
+// // console.log(fullname);
+// // // you can message me on this email,olusholaabiodun01@gmail.com
+// // const Email = " you can message me on this email " + email
+// // console.log(Email);
+// // // Getting Characters -
+// // console.log(middleName[1]);
+// // //  string lenght
+// // console.log(firstName.length);
+// // // String Method
+// // // transform
+// // console.log(fullname.toUpperCase());
+// // console.log(fullname.toLowerCase());
 
-// // extracting parts of a string - slice substring
-// const email2 = "olushola@tevhstudio.com";
-// // string.slice[0, 8]
-// console.log(email2.slice(0, 8));
-// console.log(email2.slice(5, 12));
+// // // extracting parts of a string - slice substring
+// // const email2 = "olushola@tevhstudio.com";
+// // // string.slice[0, 8]
+// // console.log(email2.slice(0, 8));
+// // console.log(email2.slice(5, 12));
 
-// // string.substring(start,counnt of cters)
-// console.log(email2.substr(0, 12));
+// // // string.substring(start,counnt of cters)
+// // console.log(email2.substr(0, 12));
 
-// // replace string content - replace reolaceALL
-// console.log(email2.replace("a", "$"));
-// console.log(email2.replaceAll("a", "$"));
+// // // replace string content - replace reolaceALL
+// // console.log(email2.replace("a", "$"));
+// // console.log(email2.replaceAll("a", "$"));
 
-// // includes,indexof, lastindexof, trim,concat,
-// // trimStart, trimend, StartsWith, Endswith
+// // // includes,indexof, lastindexof, trim,concat,
+// // // trimStart, trimend, StartsWith, Endswith
 
-// console.log(email2.includes("olushola"));
-// console.log(email2.indexOf("a"));
-// console.log(email2.lastIndexOf("a"));
-// const user = "       olushola    ";
-// const password = "       12334568   ";
-// console.log(user.length, password.length);
-// const formattedUser = user.trim();
-// console.logformattedpassword = password.trimStart();
-// console.log("formattedpassword.length");
-// console.log(email2.startsWith("o"));
-// console.log(email2.endsWith(".com"));
+// // console.log(email2.includes("olushola"));
+// // console.log(email2.indexOf("a"));
+// // console.log(email2.lastIndexOf("a"));
+// // const user = "       olushola    ";
+// // const password = "       12334568   ";
+// // console.log(user.length, password.length);
+// // const formattedUser = user.trim();
+// // console.logformattedpassword = password.trimStart();
+// // console.log("formattedpassword.length");
+// // console.log(email2.startsWith("o"));
+// // console.log(email2.endsWith(".com"));
 
-// // concat -appendix end eg
-// const job = "frontend"
-// // frontend developer
-// console.log(job.concat(" ", "Developer", " is Rare"));
-
-
-// //  template literal //zainab
-// const author = "Chinua Achebe";
-// const book = " Things Fall Apart";
-// const years = 1996;
-// // The book things Fall apart was wrtten by Chinua Achebe in the year 1996
-// const bookdescription =" The book " + book + " was written by " + author + " in the year " + years
-
-// console.log(bookdescription);
-// const bookdescription2 = `The book ${book} was written by ${author} in the year ${years}`
-
-// // numbers
-// maths operators- + - * / ** %
-console.log(6 % 2); // 0
-console.log(5 % 2); // 1
-
-console.log(5 / 2); //2.5
-// order of operation
-// increase te value of total by 10
-let total = 50;
-total = total + 10;
-console.log(total);
-
-let likes = 0;
-likes += 1;
-likes += 1;
-likes += 1;
-likes++;
-likes--;
-console.log(likes);
-
-// // boolean -True and false
-console.log(true);
-
-const country = "nigeria"
-const continent = " africa"
-let population = "106447400000"
-console.log(country, continent, population);
-const isIsland = false
-const language = "English";
-population /= 2;
-console.log(population);
-population++;
-console.log(population);
-
-// template literals
-// description nigeria is in africa and its population of 106447400000 people speaks english
-
-const description = ` ${country} is in ${continent} and its population of ${population} people speaks ${language} `
-console.log(description);
-// nigeria as a country speaks english
-const descript = `${country} as a country speaks majorly ${language}`;
-console.log(descript);
-//   logical operators - < > <= >= ==
-// = assignment
-// == loose equality, ===strict
-
-// logical operators and &&  or || not !
-console.log(5 > 6 && 2 == 2);
-
-console.log(6 > 7 || 2 === 2);
+// // // concat -appendix end eg
+// // const job = "frontend"
+// // // frontend developer
+// // console.log(job.concat(" ", "Developer", " is Rare"));
 
 
-// type conversion and con coercion
-// strings to numbers
-const str = "90";
-console.log(typeof str);
-const converted = Number(str);
-console.log(typeof converted);
+// // //  template literal //zainab
+// // const author = "Chinua Achebe";
+// // const book = " Things Fall Apart";
+// // const years = 1996;
+// // // The book things Fall apart was wrtten by Chinua Achebe in the year 1996
+// // const bookdescription =" The book " + book + " was written by " + author + " in the year " + years
 
-//  numbers to strings
-const num = 900000;
-console.log(typeof num);
-const converted2 = String(num);
-console.log(typeof converted2);
+// // console.log(bookdescription);
+// // const bookdescription2 = `The book ${book} was written by ${author} in the year ${years}`
 
-// type coercion
-const num1 = 8;
-const num2 = 2;
-const result = num1 / num2
+// // // // numbers
+// // // maths operators- + - * / ** %
+// // console.log(6 % 2); // 0
+// // console.log(5 % 2); // 1
 
-console.log(num1 + num2);
+// // console.log(5 / 2); //2.5
+// // // order of operation
+// // // increase te value of total by 10
+// // let total = 50;
+// // total = total + 10;
+// // console.log(total);
+
+// // let likes = 0;
+// // likes += 1;
+// // likes += 1;
+// // likes += 1;
+// // likes++;
+// // likes--;
+// // console.log(likes);
+
+// // // // boolean -True and false
+// // console.log(true);
+
+// // const country = "nigeria"
+// // const continent = " africa"
+// // let population = "106447400000"
+// // console.log(country, continent, population);
+// // const isIsland = false
+// // const language = "English";
+// // population /= 2;
+// // console.log(population);
+// // population++;
+// // console.log(population);
+
+// // // template literals
+// // // description nigeria is in africa and its population of 106447400000 people speaks english
+
+// // const description = ` ${country} is in ${continent} and its population of ${population} people speaks ${language} `
+// // console.log(description);
+// // // nigeria as a country speaks english
+// // const descript = `${country} as a country speaks majorly ${language}`;
+// // console.log(descript);
+// // //   logical operators - < > <= >= ==
+// // // = assignment
+// // // == loose equality, ===strict
+
+// // // logical operators and &&  or || not !
+// // console.log(5 > 6 && 2 == 2);
+
+// // console.log(6 > 7 || 2 === 2);
+
+
+// // // type conversion and con coercion
+// // // strings to numbers
+// // const str = "90";
+// // console.log(typeof str);
+// // const converted = Number(str);
+// // console.log(typeof converted);
+
+// // //  numbers to strings
+// // const num = 900000;
+// // console.log(typeof num);
+// // const converted2 = String(num);
+// // console.log(typeof converted2);
+
+// // // type coercion
+// // const num1 = 8;
+// // const num2 = 2;
+// // const result = num1 / num2
+
+// // console.log(num1 + num2);
+
+// //  Null
+// //  Undefined - undefined
+// // let age = null
+// // console.log(age, );
 
 
 
+// // truthy and falsy value
+// // truthy -
+// //  and falsy values 0, null, undefined
 
+// // control flow
+// // conditional statement
 
-// // Null
-// // Undefined
+// // // if(condition){code}
+// // const password = "12345678"
+// // if (password.length > 6) {
+// // //     console.log("your password is strong");
+// // }
+
+// // if else
+// // if(condition){
+//     // code 1
+// //}else{
+// //code 2
+// // }
+// // 
+// // if (passsword.length > 6)
+    
+    
+    
+
+// // const country = "nigeria";
+// // const continent = "africa";
+// // const population = 5000000;
+// // const isIsland = false;
+// // const language = "english";
+
+// // // sarah speaks english has less than 50 people and is not an island
+// // // you should live in portugal
+// // // portugal does not meet your criteria
+// // if (language === "english" && population < 5000000000 && !isIsland) {
+// //     console.log(`you should live in ${country}`);
+// // } else {
+// //     console.log(`${country} does not meet your criteria`);
+// // }
+
+// // const age = 18;
+// // // >= 18
+// // if (age >= 18) { console.log("you are an adult"); } else {
+// //     console.log("you are not one");
+// // }
+
+// //
+// //  if (condition)else if(another condition)else if(condition){}else{}
+
+// const password = "23345566@";
+
+// const numb = 9
+// if (numb >= 0) console.log( `${numb} is a positive number`);
+// else if (numb < 0) { console.log(`${numb} is a negative number`); }
+// else { console.log("this is zero"); }
+
+// //  EVEN OR ODD
+
+// if (numb % 2 === 0) {
+//     console.log("this is even");
+// } else {
+//     console.log("this is odd");
+// }
+
+const agee = 34
+if (agee < 18) { console.log("welcome to the baby section"); } 
+else if (agee <= 50) { console.log("welcome to the club"); }
+else
+{ console.log("welcome to the vip section"); }
+
+// SWITCH STATEMENT
+const day = 7;
+switch (day) {
+    case 1:
+        console.log("MONDAY");
+        break;
+    case 2:
+        console.log("TUESDAY");
+        break;
+    case 3:
+        console.log("WEDNESDAY");
+        break;
+    case 4:
+        console.log("THURSDAY");
+        break;
+    case 5:
+        console.log("FRIDAY");
+        break;
+    case 6:
+        console.log("SATURDAY");
+        break;
+    case 7:
+        console.log("SUNDAY");
+        break;
+        default:
+        console.log("NOT A VALID DAY");
+        break;
+}
+    
+
+const grade = "a";
+switch (grade) {
+    case "A":
+    case "a":
+        console.log("EXCELLENT");
+        break;
+    case "B":
+          
+        console.log("VERY GOOD");
+        break;
+    case "C":
+         
+        console.log("GOOD");
+        break;
+    case "D": 
+         
+        console.log("FAIR");
+        break;
+    case "E":
+        
+        console.log("POOR");
+        break;
+    default:
+        console.log("NOT A VALID GRADE");
+        break;
+        
+}
